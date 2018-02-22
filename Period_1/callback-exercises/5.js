7af6e973e21053ae409112dc619e6574cd2df552
petcat <catanapetru@gmail.com>
2018-02-22 14:26:33 +0100

    cb

--- Period_1/callback-exercises/5.js
+++ Period_1/callback-exercises/5.js
@@ -1,38 +1,38 @@
-// // a)------------------------------------------------------->>> Reduce an array to a string
-// var all = ["Lars", 'Peter', 'Jan', 'Bo'];
+// a)------------------------------------------------------->>> Reduce an array to a string
+var all = ["Lars", 'Peter', 'Jan', 'Bo'];
 
-// console.log(all.join(', #'));
+console.log(all.join(', #'));
 
 // b)------------------------------------------------------->>> Creating own reduce
-// var numbers = [2, 3, 67, 33];
+var numbers = [2, 3, 67, 33];
 
-// var sum = numbers.reduce((a, c) => {
-//     return a + c;
-// });
-// console.log(sum);
+var sum = numbers.reduce((a, c) => {
+    return a + c;
+});
+console.log(sum);
 
 // find average
-// var members = [
-//     {name : "Peter", age: 18},
-//     {name : "Jan", age: 35},
-//     {name : "Janne", age: 25},
-//     {name : "Martin", age: 22},
-//    ];
+var members = [
+    {name : "Peter", age: 18},
+    {name : "Jan", age: 35},
+    {name : "Janne", age: 25},
+    {name : "Martin", age: 22},
+   ];
 
-// var average = members.reduce((a, c, i, arr) => {
-//     return a + c.age / arr.length;
-// }, 0);
-// console.log(average);
+var average = members.reduce((a, c, i, arr) => {
+    return a + c.age / arr.length;
+}, 0);
+console.log(average);
 
 // d) ------------------------------------------------------->>> Counting votes
-// var votes = [ "Clinton","Trump","Clinton","Clinton","Trump","Trump","Trump","None"];
+var votes = [ "Clinton","Trump","Clinton","Clinton","Trump","Trump","Trump","None"];
 
-// var countedVotes = votes.reduce((allNames, name) => {
-//     if (name in allNames) {
-//         allNames[name]++;
-//     } else {
-//         allNames[name] = 1;
-//     }
-//     return allNames;
-// }, {});
-// console.log(countedVotes);
+var countedVotes = votes.reduce((allNames, name) => {
+    if (name in allNames) {
+        allNames[name]++;
+    } else {
+        allNames[name] = 1;
+    }
+    return allNames;
+}, {});
+console.log(countedVotes);
